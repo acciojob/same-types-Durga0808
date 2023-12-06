9517163843ba8784b0a55109b054542f853d3533
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-07 02:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function isSameType(value1, value2) {
   //your js code here
-	if(typeof(value1) == typeof(value2)) {
-		return true;
-	}
-	else {
-		return false;
-	}
+	 if(typeof value2=== typeof value1){
+	  return true;
+ }
+  else{
+	  return false
+  }
 }
 
 // do not change the code below.
